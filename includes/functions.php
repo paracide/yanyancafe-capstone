504e9e76d29aaa4037147aebc21981c05f3616d5
Arthur <paracidex@gmail.com>
2024-04-30 13:15:21 -0500

    feat(Lab3): functions.php

---
+++ includes/functions.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * esc all string data for output
+ *
+ * @param  string  $str
+ *
+ * @return string
+ */
+function esc(string $str): string
+{
+    return htmlentities($str, ENT_QUOTES, 'UTF-8');
+}
+
+/**
+ *  esc all string data without quotes for output
+ *
+ * @param  string  $str
+ *
+ * @return string
+ */
+function escHTML(string $str): string
+{
+    return htmlentities($str, ENT_NOQUOTES, 'UTF-8');
+}
+
+/**
+ * debug single var
+ *
+ * @param $var
+ * @param  false  $die
+ *
+ * @return void
+ */
+function dd($var, $die = false): void
+{
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+    if ($die) {
+        exit();
+    }
+}
+
+/**
+ * @param $str
+ *
+ * @return string
+ */
+function raw($str): string
+{
+    return $str;
+}
+
