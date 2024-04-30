78ab351c74e7c28508a525ded7b8baf6c3b7b727
Arthur <paracidex@gmail.com>
2024-04-30 13:37:55 -0500

    feat(Lab3): index.php with header.inc.php and footer.inc.php

---
+++ includes/header.inc.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Yanyan Cafe - Where sunbeams meet purrs, and the world shrinks to soft paw prints, a haven awaits, woven from stories and
+            sleep." name="description">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Oswald:wght@400;700&display=swap"
+      rel="stylesheet">
+    <title>Yanyan Cafe - Home</title>
+
+    <!--favicon-->
+    <link href="favicon.png" rel="icon" type="image/x-icon">
+    <link href="favicon-192.png" rel="apple-touch-icon" sizes="192x192">
+    <link href="favicon-180.png" rel="apple-touch-icon" sizes="180x180">
+    <link href="favicon-167.png" rel="apple-touch-icon" sizes="167x167">
+    <link href="favicon-152.png" rel="apple-touch-icon" sizes="152x152">
+    <link href="favicon-128.png" rel="apple-touch-icon" sizes="128x128">
+    <link href="favicon.png" rel="apple-touch-icon" sizes="32x32">
+
+    <link href="styles/base.css" media="screen" rel="stylesheet">
+    <link href="styles/mobile.css" media="screen" rel="stylesheet">
+    <link href="styles/print.css" media="print" rel="stylesheet">
+      <?php
+      echo ($title == 'Yanyan Cafe') ? '<link href="styles/index.css" media="screen" rel="stylesheet">' : ''; ?>
+
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <img alt="yanyan cafe" height="" id="logo" src="images/logo.png">
+      </header>
+      <nav class="transition shadow">
+        <div class="transition">
+          <div class="menu-shape" id="menu-top"></div>
+        </div>
+        <a class="home-menu" href="index.php"
+           title="Yanyan Cafe - Home">Home</a>
+        <a class="second-menu" href="cats.php"
+           title="Yanyan Cafe - Cats">Cats</a>
+        <a class="second-menu" href="menu.php"
+           title="Yanyan Cafe - Menu">Menu</a>
+        <a class="second-menu" href="about.php"
+           title="Yanyan Cafe - About">About</a>
+        <a class="second-menu" href="club.php"
+           title="Yanyan Cafe - Club">Club</a>
+      </nav>
--- public/index.php
+++ public/index.php
@@ -1,169 +1,106 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Yanyan Cafe - Where sunbeams meet purrs, and the world shrinks to soft paw prints, a haven awaits, woven from stories and
-            sleep." name="description">
-    <link
-      href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Oswald:wght@400;700&display=swap"
-      rel="stylesheet">
-    <title>Yanyan Cafe - Home</title>
+<?php
 
-    <!--favicon-->
-    <link href="favicon.png" rel="icon" type="image/x-icon">
-    <link href="favicon-192.png" rel="apple-touch-icon" sizes="192x192">
-    <link href="favicon-180.png" rel="apple-touch-icon" sizes="180x180">
-    <link href="favicon-167.png" rel="apple-touch-icon" sizes="167x167">
-    <link href="favicon-152.png" rel="apple-touch-icon" sizes="152x152">
-    <link href="favicon-128.png" rel="apple-touch-icon" sizes="128x128">
-    <link href="favicon.png" rel="apple-touch-icon" sizes="32x32">
+require __DIR__.'/../includes/functions.php';
+$title = 'Yanyan Cafe';
+// view starts
+require __DIR__.'/../includes/header.inc.php';
+?>
+<main>
+  <div class="intro">
+    <h1 class="gradient-anime">Yanyan Cafe</h1>
+    <p>
+      Where sunbeams meet purrs, and the world shrinks to soft paw prints,
+      a haven awaits, woven from stories and
+      sleep.
+    </p>
+  </div>
 
-    <link href="styles/base.css" media="screen" rel="stylesheet">
-    <link href="styles/mobile.css" media="screen" rel="stylesheet">
-    <link href="styles/print.css" media="print" rel="stylesheet">
-    <link href="styles/index.css" media="screen" rel="stylesheet">
-  </head>
-  <body>
-    <div class="wrapper">
-      <header>
-        <img alt="yanyan cafe" height="" id="logo" src="images/logo.png">
-      </header>
-      <nav class="transition shadow">
-        <div class="transition">
-          <div class="menu-shape" id="menu-top"></div>
-        </div>
-        <a class="home-menu" href="index.php"
-           title="Yanyan Cafe - Home">Home</a>
-        <a class="second-menu" href="cats.php"
-           title="Yanyan Cafe - Cats">Cats</a>
-        <a class="second-menu" href="menu.php"
-           title="Yanyan Cafe - Menu">Menu</a>
-        <a class="second-menu" href="about.php" title="Yanyan Cafe - About">About</a>
-        <a class="second-menu" href="club.php"
-           title="Yanyan Cafe - Club">Club</a>
-      </nav>
-      <main>
-        <div class="intro">
-          <h1 class="gradient-anime">Yanyan Cafe</h1>
-          <p>
-            Where sunbeams meet purrs, and the world shrinks to soft paw prints,
-            a haven awaits, woven from stories and
-            sleep.
-          </p>
-        </div>
+  <div class="home-cats-container page">
+    <h2>CATS</h2>
+    <p>Meet our exclusive "Cat Royalty" members, carefully chosen for
+      enchanting companionship.</p>
+    <div class="home-cat-card-group">
+      <div class="home-cat-card border shadow" id="xiuxiu">
+        <img alt="xiuxiu" src="images/home/xiuxiu.jpg">
+        <div class="home-card-text">
+          <div class="home-cat-title border">Baby
 
-        <div class="home-cats-container page">
-          <h2>CATS</h2>
-          <p>Meet our exclusive "Cat Royalty" members, carefully chosen for
-            enchanting companionship.</p>
-          <div class="home-cat-card-group">
-            <div class="home-cat-card border shadow" id="xiuxiu">
-              <img alt="xiuxiu" src="images/home/xiuxiu.jpg">
-              <div class="home-card-text">
-                <div class="home-cat-title border">Baby
-
-                </div>
-                <div class="card-line"></div>
-                <a class="button flash" href="cats.php#xiuxiu-page"
-                   title="Cats xiuxiu">Xiuxiu</a>
-              </div>
-            </div>
-            <div class="home-cat-card border shadow" id="huabi">
-              <img alt="huabi" src="images/home/huabi.jpg">
-              <div class="home-card-text">
-                <div class="home-cat-title border">Evil</div>
-                <div class="card-line"></div>
-                <a class="button  flash" href="cats.php#huabi-page"
-                   title="Cats huabi">Huabi</a>
-              </div>
-            </div>
-            <div class="home-cat-card border shadow" id="zhangmeili">
-              <img alt="zhangmeili" src="images/home/zhangmeili.jpg">
-              <div class="home-card-text">
-                <div class="home-cat-title border">Angel</div>
-                <div class="card-line"></div>
-                <a class="button  flash" href="cats.php#ml-page"
-                   title="Cats zhangmeili">Zhangmeili</a>
-              </div>
-            </div>
-            <div class="home-cat-card border shadow" id="pipi">
-              <img alt="pipi" src="images/home/pipi.jpg">
-              <div class="home-card-text">
-                <div class="home-cat-title border">Silly</div>
-                <div class="card-line"></div>
-                <a class="button  flash" href="cats.php#pipi-page"
-                   title="Cats pipi">Pipi</a>
-              </div>
-            </div>
           </div>
+          <div class="card-line"></div>
+          <a class="button flash" href="cats.php#xiuxiu-page"
+             title="Cats xiuxiu">Xiuxiu</a>
         </div>
-
-        <div class="home-menu-container page">
-          <h2>MENU</h2>
-
-          <p id="menu-intro">Indulge in a delightful culinary experience with
-            YanYan Cafe's innovative menu, featuring
-            carefully
-            curated beverages and snacks.
-          </p>
-          <div class="border menu-img-container transition" id="coffee">
-            <h3>COFFEE</h3>
-            <a class="button flash" href="menu.php#coffee"
-               title="Yanyan Cafe - Menu Coffee">Explore</a>
-          </div>
-          <div class="border menu-img-container transition" id="donut">
-            <h3>SNACK</h3>
-            <a class="button flash" href="menu.php#snack"
-               title="Yanyan Cafe - Menu Snack">Explore</a>
-          </div>
-
-          <div class="border menu-img-container transition" id="beverage">
-            <h3>BEVERAGES</h3>
-            <a class="button flash" href="menu.php#beverage"
-               title="Yanyan Cafe - Menu Beverages">Explore</a>
-          </div>
+      </div>
+      <div class="home-cat-card border shadow" id="huabi">
+        <img alt="huabi" src="images/home/huabi.jpg">
+        <div class="home-card-text">
+          <div class="home-cat-title border">Evil</div>
+          <div class="card-line"></div>
+          <a class="button  flash" href="cats.php#huabi-page"
+             title="Cats huabi">Huabi</a>
         </div>
-
-        <div class="home-us-container page">
-          <div class="home-us-wrapper">
-            <h2>ABOUT US</h2>
-            <p>Welcome to YanYan Cafe, where passion for felines meets the love
-              for a good cuppa! Nestled in the heart
-              of Winnipeg, we are a cozy space dedicated to creating memorable
-              moments with our charming cats.
-            </p>
-            <div id="us-img">
-              <img alt="us" class="border" src="images/home/us.jpg">
-            </div>
-          </div>
+      </div>
+      <div class="home-cat-card border shadow" id="zhangmeili">
+        <img alt="zhangmeili" src="images/home/zhangmeili.jpg">
+        <div class="home-card-text">
+          <div class="home-cat-title border">Angel</div>
+          <div class="card-line"></div>
+          <a class="button  flash" href="cats.php#ml-page"
+             title="Cats zhangmeili">Zhangmeili</a>
         </div>
-      </main>
-      <footer>
-        <div class="follow-us">
-          <div>Follow us for daily doses of feline charm!</div>
-          <div class="social-media">
-            <a href="#" title="Yanyan Cafe - Facebook">
-              <img alt="facebook" src="images/social-media/facebook.svg">
-            </a>
-            <a href="#" title="Yanyan Cafe - Twitter">
-              <img alt="twitter" src="images/social-media/twitter.svg">
-            </a>
-            <a href="#" title="Yanyan Cafe - Youtube">
-              <img alt="youtube" src="images/social-media/youtube.svg">
-            </a>
-          </div>
+      </div>
+      <div class="home-cat-card border shadow" id="pipi">
+        <img alt="pipi" src="images/home/pipi.jpg">
+        <div class="home-card-text">
+          <div class="home-cat-title border">Silly</div>
+          <div class="card-line"></div>
+          <a class="button  flash" href="cats.php#pipi-page"
+             title="Cats pipi">Pipi</a>
         </div>
-        <div class="copyright">Copyright &copy;2023 - Night Fae Studio</div>
-        <div class="contact-us">
-          <div>paracidex@gmail.com</div>
-          <div id="last_modified">
-            <script>
-              document.scripts[document.scripts.length - 1].parentNode.innerHTML = 'Last Modified: ' + document.lastModified;
-            </script>
-          </div>
-        </div>
-      </footer>
+      </div>
     </div>
-  </body>
-</html>
+  </div>
+
+  <div class="home-menu-container page">
+    <h2>MENU</h2>
+
+    <p id="menu-intro">Indulge in a delightful culinary experience with
+      YanYan Cafe's innovative menu, featuring
+      carefully
+      curated beverages and snacks.
+    </p>
+    <div class="border menu-img-container transition" id="coffee">
+      <h3>COFFEE</h3>
+      <a class="button flash" href="menu.php#coffee"
+         title="Yanyan Cafe - Menu Coffee">Explore</a>
+    </div>
+    <div class="border menu-img-container transition" id="donut">
+      <h3>SNACK</h3>
+      <a class="button flash" href="menu.php#snack"
+         title="Yanyan Cafe - Menu Snack">Explore</a>
+    </div>
+
+    <div class="border menu-img-container transition" id="beverage">
+      <h3>BEVERAGES</h3>
+      <a class="button flash" href="menu.php#beverage"
+         title="Yanyan Cafe - Menu Beverages">Explore</a>
+    </div>
+  </div>
+
+  <div class="home-us-container page">
+    <div class="home-us-wrapper">
+      <h2>ABOUT US</h2>
+      <p>Welcome to YanYan Cafe, where passion for felines meets the love
+        for a good cuppa! Nestled in the heart
+        of Winnipeg, we are a cozy space dedicated to creating memorable
+        moments with our charming cats.
+      </p>
+      <div id="us-img">
+        <img alt="us" class="border" src="images/home/us.jpg">
+      </div>
+    </div>
+  </div>
+</main>
+<?php
+require __DIR__.'/../includes/footer.inc.php'; ?>
+
