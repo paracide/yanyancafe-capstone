e03eae40e91732428fd854a7d300304a88637c01
Arthur <paracidex@gmail.com>
2024-05-05 19:46:42 -0500

    feat(system): error page style

--- public/error.php
+++ public/error.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once __DIR__.'/../includes/config.php';
-$title = '502';
-$desc  = 'Some error happened in System';
+$title       = '502 - Error';
+$desc        = 'Some Errors happened';
+$cssFileName = 'error';
+
 // view starts
 require_once __DIR__.'/../includes/header.inc.php';
 
