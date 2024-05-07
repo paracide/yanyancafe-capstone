98e169289b42270305d1e5f9201a0e7973010665
Arthur <paracidex@gmail.com>
2024-05-07 14:13:59 -0500

    fix(profile): Profile UI

--- public/profile.php
+++ public/profile.php
@@ -18,8 +18,76 @@ require_once __DIR__.'/../includes/header.inc.php';
 ?>
 
 <div class="page">
-  <div><?= esc($user['email']) ?></div>
-
+  <div class="profile">
+    <div class="profile-img">
+      <img src="images/avatar.webp" alt="avatar">
+      <h2><?= esc($user['first_name'].' '.$user['last_name']) ?></h2>
+    </div>
+    <div class="info card">
+      <div class="card-content">
+        <h2 class="card-title">Personal Info</h2>
+        <div class="card-text">
+            <?php
+            if ( ! empty($user['first_name'])): ?>
+              <div><b>First Name:</b> <?= esc($user['first_name']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['last_name'])): ?>
+              <div><b>Last Name:</b> <?= esc($user['last_name']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['email'])): ?>
+              <div><b>Email:</b> <?= esc($user['email']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['birthday'])): ?>
+              <div><b>Birthday:</b> <?= esc($user['birthday']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['phone'])): ?>
+              <div><b>Phone:</b> <?= esc($user['phone']) ?></div>
+            <?php
+            endif; ?>
+        </div>
+      </div>
+    </div>
+    <div class="info card">
+      <div class="card-content">
+        <h2 class="card-title">Addresses</h2>
+        <div class="card-text">
+            <?php
+            if ( ! empty($user['street'])): ?>
+              <div><b>Street:</b> <?= esc($user['street']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['city'])): ?>
+              <div><b>City:</b> <?= esc($user['city']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['province'])): ?>
+              <div><b>Province:</b> <?= esc($user['province']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['country'])): ?>
+              <div><b>Country:</b> <?= esc($user['country']) ?></div>
+            <?php
+            endif; ?>
+            <?php
+            if ( ! empty($user['postal_code'])): ?>
+              <div><b>Postal Code:</b> <?= esc($user['postal_code']) ?></div>
+            <?php
+            endif; ?>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </div>
 
