c523105fdad468100a6743135d97cdfa9cba7977
Arthur <paracidex@gmail.com>
2024-06-02 23:06:56 -0500

    feat: log all page get request

--- app/tools/LogUtils.php
+++ app/tools/LogUtils.php
@@ -1,8 +1,33 @@
 <?php
 
 namespace App\tools;
-class LogUtils{
 
+class LogUtils
+{
 
+    public static function success200(): string
+    {
+        return self::generateEvent(200);
+    }
+
+    public static function error500(): string
+    {
+        return self::generateEvent(500);
+    }
+
+    public static function error405(): string
+    {
+        return self::generateEvent(405);
+    }
+
+    public static function generateEvent(string $statusCode): string
+    {
+        $date          = date('Y-m-d H:i:s');
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        $requestUri    = $_SERVER['REQUEST_URI'];
+        $userAgent     = $_SERVER['HTTP_USER_AGENT'];
+
+        return "$date | $requestMethod | $requestUri | $statusCode | $userAgent";
+    }
 
 }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 // Front Controller
 
+global $databaseLogger, $fileLogger;
+use App\tools\LogUtils;
 use App\tools\Router;
 
 require __DIR__ . '/../config/config.php';
@@ -9,10 +11,19 @@ require __DIR__ . '/../config/config.php';
 $allowed = array_map(fn($router) => $router->name, Router::cases());
 $page    = $_REQUEST['p'] ?? '';
 if (empty($page)) {
+    $event = LogUtils::success200();
+    logEvent($databaseLogger, $event);
+    logEvent($fileLogger, $event);
     include __DIR__ . '/../app/controller/index.php';
 } elseif (in_array($page, $allowed, true)) {
+    $event = LogUtils::success200();
+    logEvent($databaseLogger, $event);
+    logEvent($fileLogger, $event);
     include __DIR__ . '/../app/controller/' . $page . '.php';
 } else {
+    $event = LogUtils::error500();
+    logEvent($databaseLogger, $event);
+    logEvent($fileLogger, $event);
     Router::go500(null);
 }
 
