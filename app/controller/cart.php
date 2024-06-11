86c53ee36a92bfa0ce93584bf0138cf06a86b086
Arthur <paracidex@gmail.com>
2024-06-11 12:58:35 -0500

    feat(frontend): tweak cart ui and border-radius size

--- app/controller/cart.php
+++ app/controller/cart.php
@@ -3,10 +3,18 @@
 use App\constant\Constant;
 use App\tools\Router;
 
-$cart  = $_SESSION[Constant::SESSION_CART] ?? [];
-$props = [
-  'title' => 'Cart',
-  'cart'  => $cart,
-];
+$cart = $_SESSION[Constant::SESSION_CART] ?? [];
 
+$subTotal = $cart ? array_reduce($cart, function ($carry, $food) {
+    return $carry + $food['totalPrice'];
+}) : 0;
+
+$props = [
+  'title'    => 'Cart',
+  'cart'     => $cart,
+  'subTotal' => $subTotal,
+  'tax'      => number_format($subTotal * 0.12, 2),
+  'total'    => number_format($subTotal * 1.12, 2),
+
+];
 Router::view('cart', $props);
--- view/cart.view.php
+++ view/cart.view.php
@@ -4,79 +4,69 @@ require_once __DIR__ . '/components/Header.php';
 ?>
 
 
-<div class="m-card mx-2 mt-16">
+<div class="m-card mx-8 mt-16">
   <div class="flex">
     <!--order details-->
     <div class="w-3/4 p-4">
       <div class="flex justify-between border-b p-4">
-        <h1 class="text-4xl">Shopping Cart</h1>
+        <h1 class="text-4xl">Cart</h1>
         <h2 class="text-2xl"><?= count($cart) ?> Items</h2>
       </div>
-      <div class="flex mt-10 mb-5">
+      <div class="flex mt-10 p-4">
         <h3 class="w-2/5">
           Product Details</h3>
         <h3
-          class="text-center w-1/5 text-center">
+          class=" w-1/5 ">
           Quantity</h3>
         <h3
-          class="text-center w-1/5 text-center">
+          class=" w-1/5 ">
           Price</h3>
         <h3
-          class="text-center w-1/5 text-center">
+          class=" w-1/5">
           Total</h3>
       </div>
-      <div class="flex items-center hover:bg-gray-100 p-5">
-
-          <?php
-          foreach ($cart as $food) : ?>
-            <div class="flex w-2/5">
-              <div class="w-20">
-                <img class="h-20 w-20 rounded" src="<?= $food['img'] ?>"
-                     alt="product">
-              </div>
-              <div class="flex flex-col justify-between ml-4 flex-grow">
-                <span class="font-bold text-sm"><?= esc($food['name']) ?></span>
-              </div>
+        <?php
+        foreach ($cart as $food) : ?>
+          <div class="flex items-center p-4">
+            <div class="flex w-2/5 gap-4 items-center">
+              <img class="h-14 w-14 rounded-xl" src="<?= $food['img'] ?>"
+                   alt="food">
+              <div class="font-bold"><?= esc($food['name']) ?></div>
             </div>
-            <div class="flex justify-center w-1/5">
-                <?= esc($food['quantity']) ?>
-            </div>
-            <span class="text-center w-1/5 ">$<?= esc($food['price']) ?></span>
-            <span class="text-center w-1/5"><?= esc(
-                  $food['totalPrice']
-                ) ?></span>
-          <?php
-          endforeach; ?>
-      </div>
+            <span class="w-1/5"><?= esc($food['quantity']) ?></span>
+            <span class=" w-1/5 ">$<?= esc($food['price']) ?></span>
+            <span class=" w-1/5"><?= esc($food['totalPrice']) ?></span>
+          </div>
+        <?php
+        endforeach; ?>
     </div>
 
     <!--cashier-->
-    <div id="summary" class="w-1/4 p-4 flex flex-col gap-8">
-      <h2 class="text-4xl border-b p-4">Order Summary</h2>
+    <div class="w-1/4 p-4 flex flex-col gap-12 glass rounded-xl">
+      <h2 class="text-4xl border-b p-4">Summary</h2>
       <div class="flex justify-between">
         <span class="text-sm uppercase">Subtotal</span>
-        <span class="text-sm">$1200</span>
+        <span class="text-sm">$<?= esc($subTotal) ?></span>
       </div>
       <div class="flex justify-between">
         <span class="text-sm uppercase">Tax</span>
-        <span class="text-sm">$1200</span>
+        <span class="text-sm">$<?= esc($tax) ?></span>
       </div>
 
-
-      <div class="border-t">
+      <div class="border-t flex flex-col gap-8">
         <div
           class="flex justify-between">
           <span>Total cost</span>
-          <span>$1210</span>
+          <span>$<?= esc($total) ?></span>
         </div>
         <button
-          class="bg-orange-600 w-full text-xl text-white btn">
+          class="bg-orange-600 <?= $cart ? ''
+            : 'btn-disabled' ?> w-full text-xl text-white btn">
           Checkout
         </button>
       </div>
     </div>
   </div>
-
 </div>
 
 
