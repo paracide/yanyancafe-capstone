a1dd4eeb1220b74bab9e1a5f892903f20e82e356
Arthur <paracidex@gmail.com>
2024-06-11 20:26:15 -0500

    feat(frontend): add info when cart is empty

--- view/cart.view.php
+++ view/cart.view.php
@@ -4,23 +4,24 @@ require_once __DIR__ . '/components/Header.php';
 ?>
 
 
-<div class="m-card mx-8 mt-16">
+<div class="m-card min-h-full mx-8 mt-16">
   <div class="flex">
     <!--order details-->
-    <div class="w-3/4 p-4">
+    <div class="w-3/4  p-4">
       <!--header-->
       <div class="flex justify-between border-b p-4">
         <h1 class="text-4xl">Cart</h1>
         <h2 class="text-2xl flex items-center gap-4">
           <form action="/?p=cart_clear_process" method="post">
-            <button type="submit" class="btn btn-success text-xl text-white">
+            <button type="submit"
+                    class="btn btn-success text-xl text-white">
               Clear
             </button>
           </form>
-            <?= count($cart) ?> Items
         </h2>
       </div>
-      <!--content-->
+
+      <!--table header-->
       <div class="flex mt-10 p-4">
         <h3 class="w-2/6">
           Product Details</h3>
@@ -37,27 +38,36 @@ require_once __DIR__ . '/components/Header.php';
           class=" w-1/6">
           Action</h3>
       </div>
+      <!--content-->
         <?php
-        foreach ($cart as $food) : ?>
-          <div class="flex items-center p-4">
-            <div class="flex w-2/6 gap-4 items-center">
-              <img class="h-14 w-14 rounded-xl" src="<?= $food['img'] ?>"
-                   alt="food">
-              <div class="font-bold"><?= esc($food['name']) ?></div>
-            </div>
-            <span class="w-1/6"><?= esc($food['quantity']) ?></span>
-            <span class=" w-1/6 ">$<?= esc($food['price']) ?></span>
-            <span class=" w-1/6"><?= esc($food['totalPrice']) ?></span>
-            <span class=" w-1/6">
+        if ( ! $cart): ?>
+          <h2 class="text-6xl">Your cart is empty</h2>
+        <?php
+        else: ?>
+            <?php
+            foreach ($cart as $food) : ?>
+              <div class="flex items-center p-4">
+                <div class="flex w-2/6 gap-4 items-center">
+                  <img class="h-14 w-14 rounded-xl" src="<?= $food['img'] ?>"
+                       alt="food">
+                  <div class="font-bold"><?= esc($food['name']) ?></div>
+                </div>
+                <span class="w-1/6"><?= esc($food['quantity']) ?></span>
+                <span class=" w-1/6 ">$<?= esc($food['price']) ?></span>
+                <span class=" w-1/6"><?= esc($food['totalPrice']) ?></span>
+                <span class=" w-1/6">
               <form action="/?p=cart_del_process" method="post">
                 <input name="menuId" value="<?= $food['id'] ?>" hidden/>
                 <button class="btn btn-warning" type="submit">DEL</button>
               </form>
             </span>
-          </div>
+              </div>
+            <?php
+            endforeach; ?>
         <?php
-        endforeach; ?>
+        endif; ?>
     </div>
+
 
     <!--cashier-->
     <div class="w-1/4 flex flex-col  glass rounded-xl gap-4 p-4">
