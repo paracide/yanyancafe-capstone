69978d0be01f36f632e9e05b4682791954d2b5c0
Arthur <paracidex@gmail.com>
2024-06-13 21:17:45 -0500

    feat(backend): menu add page file and textarea ui

--- view-admin/menu_add.view.php
+++ view-admin/menu_add.view.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/components/Header.php';
             action="/admin?menu_add_process">
         <div class="grid grid-cols-2 gap-4">
           <!-- Name -->
-          <div>
+          <div class="max-w-lg">
             <label class="input input-bordered flex items-center gap-2">
               Name
               <input type="text" name="name" class="grow"
@@ -22,9 +22,9 @@ require_once __DIR__ . '/components/Header.php';
             <span class="hidden text-sm text-red-600">Error</span>
           </div>
           <!-- Category ID -->
-          <div class="w-full">
+          <div class="max-w-lg">
             <label class="input input-bordered flex items-center gap-2">
-            <span>Category</span>
+              <span>Category</span>
               <select class="w-full appearance-none focus:outline-none">
                 <option disabled selected>Who shot first?</option>
                 <option>Han Solo</option>
@@ -33,16 +33,16 @@ require_once __DIR__ . '/components/Header.php';
             </label>
           </div>
           <!-- Price -->
-          <div>
+          <div class="max-w-lg">
             <label class="input input-bordered flex items-center gap-2">
               Price
-              <input type="number" name="price" step="0.01"
+              <input type="text" name="price"
                      placeholder="Enter price">
             </label>
             <span class="hidden text-sm text-red-600">Error</span>
           </div>
           <!-- Size -->
-          <div class="w-full">
+          <div class="max-w-lg">
             <label class="input input-bordered flex items-center gap-2">
               <span>Size</span>
               <select class="w-full appearance-none focus:outline-none">
@@ -54,21 +54,20 @@ require_once __DIR__ . '/components/Header.php';
           </div>
 
           <!-- Discount -->
-          <div>
+          <div class="max-w-lg">
             <label class="input input-bordered flex items-center gap-2">
               Discount
-              <input type="number" name="discount" step="0.01"
+              <input type="text" name="discount"
                      placeholder="Enter discount">
             </label>
             <span class="hidden text-sm text-red-600">Error</span>
           </div>
 
           <!-- Calorie Count -->
-          <div>
+          <div class="max-w-lg">
             <label class="input input-bordered flex items-center gap-2">
               Calorie Count
-              <input type="number" name="calorie_count"
-
+              <input type="text" name="calorie_count"
                      placeholder="Enter calorie count">
             </label>
             <span class="hidden text-sm text-red-600">Error</span>
@@ -77,22 +76,29 @@ require_once __DIR__ . '/components/Header.php';
           <div class="form-control w-full flex-row justify-between">
             <label class="cursor-pointer label gap-3">
               <span class="label-text">Available</span>
-              <input type="checkbox" class="toggle toggle-primary" checked />
-            </label>
-
-            <label class="cursor-pointer label gap-3">
-              <span class="label-text">Takeaway</span>
-              <input type="checkbox" class="toggle toggle-primary" checked />
+              <input type="checkbox" class="toggle toggle-primary" checked/>
             </label>
 
 
           </div>
-        </div>
-        <!-- Submit Button -->
-        <div class="flex w-full gap-4">
-          <button type="submit" class="btn btn-primary flex-1">Add Menu Item
+          <div class="form-control w-full flex-row justify-between">
+            <label class="cursor-pointer label gap-3">
+              <span class="label-text">Takeaway</span>
+              <input type="checkbox" class="toggle toggle-primary" checked/>
+            </label>
+          </div>
+
+          <input type="file"
+                 class="file-input file-input-bordered file-input-secondary w-full max-w-lg"/>
+
+          <textarea class="textarea textarea-bordered  max-w-lg"
+                    placeholder="Description"></textarea>
+
+          <button type="submit" class="btn btn-primary flex-1  max-w-lg">Add
           </button>
-          <button type="reset" class="btn btn-secondary flex-1">Reset</button>
+          <button type="reset" class="btn btn-secondary flex-1  max-w-lg">
+            Reset
+          </button>
         </div>
       </form>
 
